14bdeb584e7752761e33b36f8062d96af7fba056
suksakk <kr.sak999@gmail.com>
2024-09-19 03:22:38 +0900

    Create User Model

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -22,7 +22,20 @@ class UserStorage {
             }
             return newUsers;
         }, {});
+        
         return newUsers;
+    }
+    
+    static getUserInfo(id) {
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users);   // => [id, password, name]
+        const userInfo = Object.keys(users).reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+        
+        return userInfo;
     }
 }
 
